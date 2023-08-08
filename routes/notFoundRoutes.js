c74339b21553ded7fd3fba26328727926773b03d
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-08 15:04:05 -0400

    removing the path since no file is being used

--- routes/notFoundRoutes.js
+++ routes/notFoundRoutes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const path = require('path');
 
 router.get("*", (req, res) => {
     res.status(404).json({ "message": "nothing to see here, please go back to homepage" });
